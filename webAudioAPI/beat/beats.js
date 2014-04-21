6acbbfe93d2e3b1e4c724c5c9c313d2375b6b900
curran <curran.kelleher@gmail.com>
2014-04-20 22:07:09 -0400

    Added note on beats

---
+++ webAudioAPI/beat/beats.js
@@ -0,0 +1,16 @@
+// Idea:
+//
+//  * background beat synthesizer
+//  * all synthesized sounds
+//    * Drums
+//    * Bass
+//  * fundamentals
+//    * Noise generator
+//    * Envelope
+//    * Event sequencer
+//    * Events
+//
+//  * multi-touch beat making interface
+//  * first goal:
+//    * 4 on the floor bass drum & hi hat
+//    * 8 bar phrase in bass
